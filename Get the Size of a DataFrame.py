de6b0d38d6b426c339df9452cca2b369c4acca07
productionIssue <163876902+productionIssue@users.noreply.github.com>
2024-05-09 21:45:00 +0530

    Update Get the Size of a DataFrame.py

--- Get the Size of a DataFrame.py
+++ Get the Size of a DataFrame.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def getDataframeSize(players: pd.DataFrame) -> List[int]:
     size_list = [0,0]
-    shape = players.values.shape
+    shape = players.shape
     size_list[0] = shape[0]
     size_list[1] = shape[1]
     return size_list
